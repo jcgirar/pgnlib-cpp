ed0d31cdfe6df60814984341e39a73b531bc779d
Nikita Manovich <nikita.manovich@gmail.com>
2010-10-30 21:41:02 +0400

    Added copyright headers in each source file in accordance with http://www.gnu.org/licenses/gpl-howto.html HOWTO.

--- include/parser.hpp
+++ include/parser.hpp
@@ -1,3 +1,25 @@
+/**
+ * Copyright (C) 2010 Nikita Manovich <nikita.manovich@gmail.com>
+ * 
+ * This file is part of C++ PGN parser, http://code.google.com/p/pgnparser-cpp
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef PGN_PARSER_HPP
+#define PGN_PARSER_HPP
+
 #include "ref_object.hpp"
 
 namespace pgn
@@ -6,8 +28,10 @@ namespace pgn
 class IParser : public IRefObject
 {
 public:
-    static IParser* create(char const* pgnfile /* utf8 */);
+    static IParser* create(char const* pgnfile);
 };
 
 } /* namespace pgn */
+
+#endif /* #ifndef PGN_PARSER_HPP */
 
